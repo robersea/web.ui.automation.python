b9515ffd00fbe5a5ce1ebbeb8b13f21e0c180e22
seanroberts <sean.roberts87@gmail.com>
2023-08-11 12:25:44 -0700

    Update login page object. Update environment json file.

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,14 +1,12 @@
-
 class LoginPage:
 
     def __init__(self, page):
         self.page = page
-        self.user_name = page.locator("example_user_name_locator")
-        self.password = page.locator("example_password_locator")
-        self.login_button = page.locator("example_login_button_locator")
+        self.user_name = page.locator("#user-name")
+        self.password = page.locator("#password")
+        self.login_button = page.locator("#login-button")
 
     def login(self, config, env, user_type):
         self.user_name.fill(config['Environments'][f'{env}']['Users'][f'{user_type}']['UserName'])
         self.password.fill(config['Environments'][f'{env}']['Users'][f'{user_type}']['Password'])
         self.login_button.click()
-
